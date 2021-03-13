1979d7bf96eee284303d306b9bafb45f7cc2c9a7
hoanggit <nguyen.d.hoang@gmail.com>
2021-03-13 11:35:45 -0800

    updating stripe integration and updating our favicon and title

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,12 +3,12 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyD4ry37rUWEYNSdapzeLBwpMvCHXNClI1c",
-    authDomain: "crwn-db-f0fe4.firebaseapp.com",
-    projectId: "crwn-db-f0fe4",
-    storageBucket: "crwn-db-f0fe4.appspot.com",
-    messagingSenderId: "1056401131267",
-    appId: "1:1056401131267:web:4bd5c0950e7775c5790b36"
+  apiKey: "AIzaSyD4ry37rUWEYNSdapzeLBwpMvCHXNClI1c",
+  authDomain: "crwn-db-f0fe4.firebaseapp.com",
+  projectId: "crwn-db-f0fe4",
+  storageBucket: "crwn-db-f0fe4.appspot.com",
+  messagingSenderId: "1056401131267",
+  appId: "1:1056401131267:web:4bd5c0950e7775c5790b36"
 };
 
 firebase.initializeApp(config);
